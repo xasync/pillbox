47a97d076aacaf6da3d3744f2422fceac057afd8
stone <stonenice@outlook.com>
2021-11-17 23:50:03 +0800

    do not reverse import

--- pillbox/__init__.py
+++ pillbox/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 import os
-import sys
 from . import pillbox_support
 from . import views
 
@@ -12,7 +11,6 @@ def create_app():
 
     app.before_request_funcs.setdefault(None, []).append(pillbox_support.login_check_interceptor)
 
-    app.register_blueprint(views.auth.bp)
-    app.register_blueprint(views.index.bp)
+    views.init_app(app)
 
     return app
